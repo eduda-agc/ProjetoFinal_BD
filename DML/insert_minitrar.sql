484d20ee8e8b16de2269ed49479c6d7625663468
Murilo_Matos <90736103+MuriloMatos2004@users.noreply.github.com>
2025-06-20 12:23:40 -0300

    Create insert_minitrar.sql

---
+++ DML/insert_minitrar.sql
@@ -0,0 +1,11 @@
+INSERT INTO Ministrar (professor_id, disciplina_id, gestao_id) VALUES
+  (101, 'ALG1',     1),
+  (102, 'CAL1',     2),
+  (103, 'ADM101',   1),
+  (104, 'BIO101',   2),
+  (105, 'ENGC101',  1),
+  (106, 'PED101',   2),
+  (107, 'MUS101',   1),
+  (108, 'LET101',   2),
+  (109, 'SOC101',   1),
+  (110, 'ART101',   2);
