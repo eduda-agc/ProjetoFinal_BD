c5d4c395b7f43137a695a1431fc6b4c398466f33
Pablo <pablo.montemor@usp.br>
2025-06-19 21:26:17 -0300

    adiciona registros às tabelas curso, departamento e disciplina

---
+++ DML/inserts_departamento.sql
@@ -0,0 +1,11 @@
+INSERT INTO Departamento (codigo, nome, chefe_id) VALUES
+  ('BIO',  'Departamento de Biologia',     4),
+  ('ADM',  'Departamento de Administração', 15),
+  ('ENGC', 'Departamento de Eng. Civil',   14),
+  ('PSI',  'Departamento de Psicologia',   17),
+  ('ECON', 'Departamento de Economia',     16),
+  ('PED',  'Departamento de Pedagogia',   18),
+  ('MUS',  'Departamento de Música',      19),
+  ('LET',  'Departamento de Letras',       5),
+  ('SOC',  'Departamento de Sociologia',   9),
+  ('ART',  'Departamento de Artes Visuais',11);
