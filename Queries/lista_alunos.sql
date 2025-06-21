6ddffad4ea80cce9880d9898d864a5b04bbe3f76
Murilo_Matos <90736103+MuriloMatos2004@users.noreply.github.com>
2025-06-21 20:59:31 -0300

    Update lista_alunos.sql

--- Queries/lista_alunos.sql
+++ Queries/lista_alunos.sql
@@ -1,18 +1,11 @@
--- Consulta para listar alunos matriculados em uma disciplina específica
 SELECT 
-    a.id AS aluno_id,
-    a.nome AS aluno_nome,
-    a.matricula,
-    d.nome AS disciplina_nome,
-    d.codigo AS disciplina_codigo
+    usuario_id AS "ID do Aluno",
+    nome_curso AS "Curso",
+    ano_ingresso AS "Ano de Ingresso"
 FROM 
-    alunos a
-JOIN 
-    matriculas m ON a.id = m.aluno_id
-JOIN 
-    disciplinas d ON m.disciplina_id = d.id
+    Aluno
 WHERE 
-    d.codigo = 'DISC123'  -- Substitua pelo código ou nome da disciplina desejada
-    -- ou: d.nome = 'Banco de Dados'
+    nome_curso = 'Ciência da Computação'
+    AND ano_ingresso = 2022
 ORDER BY 
-    a.nome;
+    usuario_id;
