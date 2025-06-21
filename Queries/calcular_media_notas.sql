144b2f79a75897ae3343a76a3f12dca38f79eaa5
Murilo_Matos <90736103+MuriloMatos2004@users.noreply.github.com>
2025-06-21 20:44:34 -0300

    Update calcular_media_notas.sql

--- Queries/calcular_media_notas.sql
+++ Queries/calcular_media_notas.sql
@@ -1,1 +1,10 @@
-
+-- Média geral de um aluno específico (substitua '123' pelo ID do aluno)
+SELECT 
+    aluno_id,
+    AVG(nota) AS media_notas
+FROM 
+    notas
+WHERE 
+    aluno_id = 123  -- Filtra por um aluno
+GROUP BY 
+    aluno_id;
