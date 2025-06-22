877e697e6b497b8f6d2f8d066a27d7e8339927fc
eduda-agc <137100218+eduda-agc@users.noreply.github.com>
2025-06-21 22:32:56 -0300

    Update top5_profs.sql
    Só mexendo para caber no print para o pdf

--- Queries/top5_profs.sql
+++ Queries/top5_profs.sql
@@ -1,4 +1,5 @@
-SELECT p.usuario_id AS professor_id, u.nome || ' ' || u.sobrenome AS professor_nome, AVG(a.nota_geral)::NUMERIC(4,2) AS media_nota_geral FROM Professor P
+SELECT p.usuario_id AS professor_id, u.nome || ' ' || u.sobrenome AS professor_nome, AVG(a.nota_geral)::NUMERIC(4,2) 
+  AS media_nota_geral FROM Professor P
 JOIN Usuario U ON P.usuario_id = U.usuario_id
 JOIN Avaliar A ON P.usuario_id = A.professor_id
 GROUP BY
