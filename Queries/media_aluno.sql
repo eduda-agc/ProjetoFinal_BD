d8827816525ab3f028fe98df2a6e3efb2613335f
chonkachu <chrissimascgr@gmail.com>
2025-06-24 22:36:10 -0300

    ajeitando algumas queries

--- Queries/media_aluno.sql
+++ Queries/media_aluno.sql
@@ -1,4 +1,5 @@
 SELECT MD.aluno_id, ROUND(AVG(mn.valor)::NUMERIC, 2) AS media_geral_notas FROM Matricula_Nota MN
 JOIN Matricula_Disciplina MD ON MN.aluno_id = MD.aluno_id AND MN.disciplina_id = MD.disciplina_id
+WHERE MN.aluno_id = 'U025'
 GROUP BY MD.aluno_id;
 
